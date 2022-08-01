1f2db19f5b876ac8af3c26a949c7b9eca99377fb
Learces <qian.haiyang@foxmail.com>
2022-08-01 17:20:56 +0800

    完成loginPage页面的基本样式

--- src/components/LoginPanel/index.tsx
+++ src/components/LoginPanel/index.tsx
@@ -45,7 +45,7 @@ const GetVerificationCodeButton: React.FC<GetVerificationCodeButtonProps> =
         onClick={
           countdown === props.countdown ? requestVerificationCode : undefined
         }
-        className={styles["getVerificationCode-button"]}
+        className={styles["getPhoneVerificationCode-button"]}
       >
         {countdown === props.countdown ? "获取验证码" : `${countdown}秒后重试`}
       </button>
@@ -60,7 +60,6 @@ const MessagePanel: React.FC = () => {
         <input
           type="text"
           placeholder="手机号"
-          maxLength={11}
           className={styles["phone-input"]}
         />
         <GetVerificationCodeButton countdown={10} />
@@ -68,8 +67,7 @@ const MessagePanel: React.FC = () => {
       <input
         type="text"
         placeholder="验证码"
-        maxLength={6}
-        className={styles.verificationCode}
+        className={styles.phoneVerificationCode}
       />
       <small className={styles.tip}>
         温馨提示：未注册硅谷外卖账号的手机号，登录时将自动注册，且代表以同意
@@ -81,9 +79,94 @@ const MessagePanel: React.FC = () => {
   );
 };
 
+// 控制密码显示样式按钮
+interface PasswordDisplaySwitchProps {
+  // 用户组件向该组件传递一个回调，该回调会受到当前开关的状态，回调可以根据当前开关的状态来完成指定动作
+  onSwitch: (state: boolean) => void;
+  // 开关初始状态
+  on?: boolean;
+}
+
+const PasswordDisplaySwitch: React.FC<PasswordDisplaySwitchProps> = props => {
+  const [isOn, setOnOrOff] = React.useState(props.on || false);
+  const [switchClassNames, setSwitchClassNames] = React.useState([
+    styles.displaySwitch,
+  ]);
+  const [controlClassNames, setControlClassNames] = React.useState([
+    styles.displayControl,
+  ]);
+  // 一定要将回调调用卸载副作用当中，不能卸载setState当中，会出Bug
+  React.useEffect(() => {
+    props.onSwitch(isOn);
+    if (isOn) {
+      setSwitchClassNames([
+        styles.displaySwitch,
+        styles["displaySwitch-active"],
+      ]);
+      setControlClassNames([
+        styles.displayControl,
+        styles["displayControl-active"],
+      ]);
+    } else {
+      setSwitchClassNames([styles.displaySwitch]);
+      setControlClassNames([styles.displayControl]);
+    }
+  }, [props, isOn]);
+  const handleClick = () => {
+    setOnOrOff(previous => !previous);
+  };
+  return (
+    <span className={switchClassNames.join(" ")} onClick={handleClick}>
+      <span className={controlClassNames.join(" ")}></span>
+    </span>
+  );
+};
+
 // 密码登录
 const PasswordPanel: React.FC = () => {
-  return <div>PasswordPanel</div>;
+  // 密码框输入类型
+  type PasswordType = "password" | "text";
+  // 初始类型为“password”类型
+  const [passwordType, setPasswordType] =
+    React.useState<PasswordType>("password");
+  // 根据开关的状态设定密码框类型，开关打开显示密码，开关关闭隐藏密码
+  const control = (state: boolean) => {
+    if (state) {
+      setPasswordType("text");
+    } else {
+      setPasswordType("password");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        className={styles.username}
+        type="text"
+        placeholder="手机/邮箱/用户名"
+      />
+      <div className={styles.password}>
+        <input
+          className={styles["password-input"]}
+          type={passwordType}
+          placeholder="密码"
+        />
+        <PasswordDisplaySwitch onSwitch={control} />
+      </div>
+      <div className={styles.imageVerificationCode}>
+        <input
+          className={styles["imageVerificationCode-input"]}
+          type="text"
+          placeholder="验证码"
+        />
+        <img
+          src="https://picsum.photos/id/993/200/200"
+          alt="验证码"
+          className={styles.verificationCodeImage}
+        />
+      </div>
+    </div>
+  );
 };
 
 // 登录面板
